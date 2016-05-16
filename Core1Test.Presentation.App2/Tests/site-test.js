9d6270aee7cabdb26b3dad82a72e34006f1a7b96
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2016-05-16 13:50:13 -0300

    Adicionando pasta App (incompleto)

---
+++ Core1Test.Presentation.App2/Tests/site-test.js
@@ -0,0 +1,19 @@
+﻿// Mocha (https://mochajs.org/) JavaScript tests for the calculator in site.js.
+describe("Tests", function () {
+
+    describe("Test", function () {
+        it("Testing.", function () {});
+    });
+
+});
+
+// An example of how to test asynchronous code.
+describe("setTimeout", function () {
+    // A 'done' function delegate must be called to tell Mocha when the test has finished.
+    it("should complete after one second.", function (done) {
+        setTimeout(function () {
+            done();
+        },
+        1000);
+    })
+});
---
+++ Core1Test.Presentation.App2/wwwroot/_references.js
@@ -0,0 +1,4 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../gulpfile.js" />
+/// <reference path="../scripts/site.js" />
+/// <reference path="../tests/site-test.js" />
