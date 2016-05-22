7722b4590ce17f37ee1d6ef9bca4c678966cf16f
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2016-05-21 22:54:03 -0300

    App renderizando index.html

---
+++ Core1Test.Presentation.App/Constants/HomeController/HomeControllerRoute.cs
@@ -0,0 +1,12 @@
+﻿namespace Core1Test.Presentation.App.Constants.HomeController
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class HomeControllerRoute
+    {
+        public const string Index = "HomeIndex";
+    }
+}
---
+++ Core1Test.Presentation.App/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿namespace Core1Test.Presentation.App.Controllers
+{
+    using System.Text;
+    using System.IO;
+    using Constants.HomeController;
+    using Microsoft.AspNet.Mvc;
+
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        [HttpGet("", Name = HomeControllerRoute.Index)]
+        public IActionResult Index()
+        {
+            using (StreamReader c = new StreamReader(new FileStream("wwwroot/index.html", FileMode.Open)))
+            {
+                return Content(c.ReadToEnd(), "text/html", Encoding.UTF8);
+            }
+        }
+    }
+}
