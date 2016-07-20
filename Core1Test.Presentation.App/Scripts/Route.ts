f8cdfc25b7eeaaa2c48549afe423e6bb2cb99d1c
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2016-07-19 21:21:04 -0300

    Configurando controller para ser resolvido como uma dependência de acordo com a rota

--- Core1Test.Presentation.App/Scripts/Route.ts
+++ Core1Test.Presentation.App/Scripts/Route.ts
@@ -41,8 +41,8 @@ define([
     };
 
     Route.SetarTodas = (): void => {
-        var name: string,
-            state: any;
+        var state,
+            name: string;
 
         for (name in Route) {
             state = Route[name];
@@ -50,6 +50,80 @@ define([
             Route.StateValidar(state) && Lazyload.$stateProvider.state(name, state);
         }
     };
+
+    Route.InjetarDependenciasResolver = (stateSimple): void => {
+        var state, resolveOld, resolveNew, resolve,
+            name: string,
+            ReordenarResolver: Function,
+            TratarResolver: Function;
+
+        ReordenarResolver = (state: any): any => {
+            var resolve,
+                newResolve: any,
+                nameResolve: string;
+
+            newResolve = {};
+            resolve = state.resolve;
+            newResolve.dependencies = resolve.dependencies;
+
+            for (nameResolve in resolve) {
+                newResolve[nameResolve] = resolve[nameResolve];
+            }
+
+            resolve = newResolve;
+
+            return state;
+        };
+
+        TratarResolver = (state): any => {
+            var innerState,
+                nameState: string;
+
+            if (Route.StateValidar(state)) {
+                resolve = state.resolve = state.resolve || {};
+
+                resolve.dependencies = ['$q', ($q): void => {
+                    var deferred;
+
+                    deferred = $q.defer();
+                    state.dependencies = state.dependencies || [];
+                    state.dependencies.unshift('angularAMD');
+
+                    require(state.dependencies, (angularAMD): void => {
+                        angularAMD.processQueue();
+
+                        deferred.resolve();
+                    });
+
+                    return deferred.promise;
+                }];
+
+                state = ReordenarResolver(state);
+
+                if (state.views) {
+                    for (nameState in state.views) {
+                        innerState = state.views[nameState];
+                        
+                        innerState = TratarResolver(innerState);
+                    }
+                }
+            }
+
+            return state;
+        };
+
+        if (stateSimple) {
+            TratarResolver(stateSimple);
+        } else {
+            for (name in Route) {
+                state = Route[name];
+
+                state = TratarResolver(state);
+            }
+        }
+    };
+
+    Route.InjetarDependenciasResolver();
 
     return Route;
 });
